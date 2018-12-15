49b2ebe90b12439a2ddbc58e5efcd8eaad6a8d2e
iamruhul <ruhulamin3482@gmail.com>
2018-12-15 21:52:30 +0600

    asyncLoad functionality added

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import PrivateRoute from '../ControlledRoute/PrivateRoute';
-import PublicRoute from '../ControlledRoute/PublicRoute';
+import PrivateRoute from "../ControlledRoute/PrivateRoute";
+import PublicRoute from "../ControlledRoute/PublicRoute";
 
-import HomePage from '../Homepage';
-import AuthPage from '../Auhtpage';
-import NotFound from '../../components/NoteFound';
+// import HomePage from '../Homepage';
+// import AuthPage from "../Auhtpage";
+// import NotFound from "../../components/NoteFound";
+import asyncLoad from "../../utils/asyncLoad";
 
 const App = () => (
   <Switch>
-    <PrivateRoute path="/" exact component={HomePage} />
-    <PublicRoute path="/auth" component={AuthPage} />
-    <Route component={NotFound} />
+    <PrivateRoute
+      path="/"
+      exact
+      component={asyncLoad(() => import("../Homepage"))}
+    />
+    <PublicRoute
+      path="/auth"
+      component={asyncLoad(() => import("../Auhtpage"))}
+    />
+    <Route component={asyncLoad(() => import("../../components/NoteFound"))} />
   </Switch>
 );
 
---
+++ src/utils/asyncLoad.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import nprogress from "nprogress";
+import "nprogress/nprogress.css";
+
+export default importComponent => props => {
+  const [loadedComponent, setComponent] = useState(null);
+
+  // this works like componentwillMount
+  if (!nprogress.isStarted()) nprogress.start();
+
+  if (loadedComponent) nprogress.done();
+
+  useEffect(() => {
+    let mounted = true;
+
+    importComponent().then(
+      ({ default: C }) => mounted && setComponent(<C {...props} />)
+    );
+    // when the component unmounts
+    return () => {
+      nprogress.done();
+      mounted = false;
+    };
+  }, []);
+
+  // return the loaded component
+  const Component = loadedComponent || <div />;
+  return Component;
+};
